85ef134859b80c283396d55a6750cd9f89c8f619
Samuel Dufour <dufoursamuel.fr@gmail.com>
2023-12-02 22:24:51 +0100

    Refactor day 2

--- day-2/answers.fsx
+++ day-2/answers.fsx
@@ -1,6 +1,5 @@
 open System.IO
 open System
-open System.Collections.Generic
 
 let filePath = $@"{__SOURCE_DIRECTORY__}\inputs.txt"
 
@@ -16,88 +15,86 @@ type Color =
         | "blue" -> Blue
         | _ -> failwith "Invalid color"
 
-type ColorsCount = Dictionary<Color, int>
+type ColorsCount = Map<Color, int>
 
-type Game =
-    { id: int
-      sets: ColorsCount list }
+type Game = { id: int; sets: ColorsCount list }
 
-    member x.isGamePossible(colorsCount: ColorsCount) =
-        let testSet (set: ColorsCount) =
-            let allColorsInBag = set.Keys |> Seq.forall (fun k -> colorsCount.ContainsKey k)
+module Game =
+    let requireColorsCount game =
+        let folder acc s =
+            let folder acc k =
+                match Map.tryFind k acc, Map.find k s with
+                | Some e, e' when e > e' -> acc
+                | _, e -> acc.Add(k, e)
+
+            s.Keys |> Seq.fold folder acc
+
+        game.sets |> Seq.fold folder Map.empty<Color, int>
+
+    let isGamePossible colorsCount game =
+        let testSet set =
+            let allColorsInBag =
+                set |> Map.keys |> Seq.forall (fun k -> colorsCount |> Map.containsKey k)
 
             let enoughColorCubesInBag =
-                set.Keys |> Seq.forall (fun k -> colorsCount[k] >= set[k])
+                set |> Map.keys |> Seq.forall (fun k -> colorsCount |> Map.find k >= set[k])
 
             allColorsInBag && enoughColorCubesInBag
 
-        x.sets |> Seq.forall testSet
+        game.sets |> Seq.forall testSet
 
-    member x.requireColorsCount() =
-        let folder (acc: Dictionary<Color, int>) (s:ColorsCount) =
-                s.Keys
-                |> Seq.iter (fun k ->
-                    if acc.ContainsKey k |> not then
-                        acc.Add(k, s[k])
-                    elif acc[k] < s[k] then
-                        acc[k] <- s[k])
-                acc
-        
-        x.sets
-        |> List.fold folder (Dictionary<Color, int>())
-
-
-module Game =
-    let toId line =
-        let digits =
-            line
-            |> Seq.skipWhile (Char.IsDigit >> not)
-            |> Seq.takeWhile Char.IsDigit
-            |> Seq.toArray
-
-        String(digits) |> int
-
-    let toDist (s: string) =
-        match s.Split([| ' ' |], StringSplitOptions.RemoveEmptyEntries) with
-        | [| n; color |] -> Color.fromColorName color, int n
-        | _ -> failwith "Invalid Distribution"
-
-    let toSet (set: string) =
-        let colors = set.Split([| ',' |], StringSplitOptions.RemoveEmptyEntries)
-        colors |> Array.map toDist |> dict |> Dictionary
-
-    let toSets (line: string) =
-        let sets = line.Split([| ';' |], StringSplitOptions.RemoveEmptyEntries)
-        sets |> Array.map toSet |> Array.toList
 
     let toGame (line: string) =
+        let toId line =
+            let digits =
+                line
+                |> Seq.skipWhile (Char.IsDigit >> not)
+                |> Seq.takeWhile Char.IsDigit
+                |> Seq.toArray
+
+            String(digits) |> int
+
+        let toSets (line: string) =
+            let toSet (set: string) =
+                let toDist (s: string) =
+                    match s.Split([| ' ' |], StringSplitOptions.RemoveEmptyEntries) with
+                    | [| n; color |] -> Color.fromColorName color, int n
+                    | _ -> failwith "Invalid Distribution"
+
+                set.Split([| ',' |], StringSplitOptions.RemoveEmptyEntries)
+                |> Array.map toDist
+                |> Map.ofArray
+
+            line.Split([| ';' |], StringSplitOptions.RemoveEmptyEntries)
+            |> Array.map toSet
+            |> Array.toList
+
         match line.Split([| ':' |], StringSplitOptions.RemoveEmptyEntries) with
-        | [|idPart; setsPart|] ->
-            let id = toId idPart
-            { id = id; sets = toSets setsPart }
+        | [| idPart; setsPart |] ->
+            { id = toId idPart
+              sets = toSets setsPart }
         | _ -> failwith "Invalid Game"
 
 module FirstAnswer =
-    let colorsBag = [Red, 12; Green, 13; Blue, 14 ] |> dict |> Dictionary
+    let colorsBag = [ Red, 12; Green, 13; Blue, 14 ] |> Map.ofList
+    let gameIsPossible = Game.isGamePossible colorsBag
 
     let toResult () =
         filePath
         |> File.ReadLines
         |> Seq.map Game.toGame
-        |> Seq.where (fun g -> g.isGamePossible colorsBag)|> Seq.map (_.id)
+        |> Seq.where gameIsPossible
+        |> Seq.map (fun x -> x.id)
         |> Seq.sum
 
 module SecondAnswer =
-    let colorsCountToValues (colorsCount: ColorsCount) =
-        colorsCount.Keys |> Seq.map (fun k -> colorsCount[k]) |> Seq.reduce (*)
+    let colorsCountToValues = Map.values >> Seq.reduce (*)
 
     let toResult () =
         filePath
         |> File.ReadLines
-        |> Seq.map Game.toGame
-        |> Seq.map (fun g -> g.requireColorsCount ())
-        |> Seq.map colorsCountToValues
+        |> Seq.map (Game.toGame >> Game.requireColorsCount >> colorsCountToValues)
         |> Seq.sum
 
-FirstAnswer.toResult()
-SecondAnswer.toResult()
+FirstAnswer.toResult ()
+SecondAnswer.toResult ()
