7e5669515e9b27845c9941974e9a2204f9dc3ed2
Samuel Dufour <dufoursamuel.fr@gmail.com>
2023-12-10 17:06:00 +0100

    Day 6 - Answer 2

--- day-6/answers.fsx
+++ day-6/answers.fsx
@@ -1,25 +1,25 @@
 open System.IO
 open System.Text.RegularExpressions
 
-let filePath = $@"{__SOURCE_DIRECTORY__}\test-input.txt"
-// let filePath = $@"{__SOURCE_DIRECTORY__}\inputs.txt"
+// let filePath = $@"{__SOURCE_DIRECTORY__}\test-input.txt"
+let filePath = $@"{__SOURCE_DIRECTORY__}\inputs.txt"
 
-type Time = Time of int
-type Distance = Distance of int
+type Time = Time of int64
+type Distance = Distance of int64
 
 type Race =
     { Time: Time
       Distance: Distance }
 
     static member compute({ Time = Time t; Distance = Distance d }) = 
-        let speed = 1
+        let speed = 1L
         let traveledDistance  raceTime waitTime = 
             let travelTime = raceTime - waitTime
             waitTime * speed * travelTime
         let traveledDistance = traveledDistance t
 
         [|
-            for ms in 0..t do
+            for ms in 0L..t do
                 traveledDistance ms
         |]
         |> Array.where (fun td -> td > d)
@@ -30,7 +30,16 @@ let (|ParseRegex|) pattern s =
     let ss = Regex.IsMatch(s, pattern)
 
     if ss then
-        m |> Seq.map (fun s -> s.Value |> int) |> Seq.toArray |> Some
+        m |> Seq.map (fun s -> s.Value |> int64) |> Seq.toArray |> Some
+    else
+        None
+
+let (|ParseRegex2|) pattern s =
+    let m = Regex.Matches(s, pattern)
+    let ss = Regex.IsMatch(s, pattern)
+
+    if ss then
+        m |> Seq.map (fun s -> s.Value) |> Seq.fold (fun acc d -> acc + d) "" |> int64 |> Some
     else
         None
 
@@ -39,35 +48,42 @@ let (|LabeledInts|_|) (label: string) (s: string) =
     | [| _; ParseRegex "(\d+)" times |] -> times |> Some
     | _ -> None
 
+let (|LabeledInt|_|) (label: string) (s: string) =
+    match s.Split(label) with
+    | [| _; ParseRegex2 "(\d+)" times |] -> times |> Some
+    | _ -> None
+
 let (|Times|_|) (s: string) =
     match s with
-    | LabeledInts "Time:" times -> times
+    | LabeledInt "Time:" times -> times
     | _ -> None
 
 let (|Distances|_|) (s: string) =
     match s with
-    | LabeledInts "Distance:" times -> times
+    | LabeledInt "Distance:" times -> times
     | _ -> None
 
 
 let toRaces lines =
     let tryToTimes (line: string) =
         match line with
-        | Times times -> times |> Array.map Time |> Some
+        | Times times -> times |> Time |> Some
         | _ -> None
 
     let tryToDistances (line: string) =
 
         match line with
-        | Distances times -> times |> Array.map Distance |> Some
+        | Distances times -> times |> Distance |> Some
         | _ -> None
 
-    let times = lines |> Array.map tryToTimes |> Array.choose id |> Array.collect id
+    let times = lines |> Array.map tryToTimes |> Array.choose id 
 
     let distances =
-        lines |> Array.map tryToDistances |> Array.choose id |> Array.collect id
+        lines |> Array.map tryToDistances |> Array.choose id 
 
     Array.zip times distances
     |> Array.map (fun (t, d) -> { Time = t; Distance = d })
 
-filePath |> File.ReadAllLines |> toRaces //|> Array.map Race.compute|> Array.reduce (*)
+filePath |> File.ReadAllLines |> toRaces |> Array.map Race.compute//|> Array.reduce (*)
+
+
